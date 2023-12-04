2dc849d24fef12e0196e3460fe66f61415861750
calotocen <calotocen@gmail.com>
2023-12-04 23:13:07 +0900

    Collected timestamps of files

--- calculate_file_digest.rb
+++ calculate_file_digest.rb
@@ -17,10 +17,24 @@ option_parser.parse!(ARGV)
 
 csv_options = {
     write_headers: true,
-    headers: ['Path', 'File Name', 'SHA-256'],
+    headers: ['Path', 'File Name', 'SHA-256', 'Created Time', 'Modified Time', 'Access Time', 'Change Time'],
 }
 csv_writer = option[:output].nil? ? CSV.new($stdout, **csv_options) : CSV.open(option[:output], 'wb', **csv_options)
-write_row = ->(path) { csv_writer << [File.expand_path(path), File.basename(path), Digest::SHA256.file(path).hexdigest] }
+write_row = ->(path) do
+    file_stat = File.lstat(path)
+    begin
+        birth_time = file_stat.birthtime
+    rescue NotImplementedError
+        birth_time = 'N/A'
+    end
+    csv_writer << [File.expand_path(path),
+                   File.basename(path),
+                   Digest::SHA256.file(path).hexdigest,
+                   birth_time,
+                   file_stat.mtime,
+                   file_stat.atime,
+                   file_stat.ctime]
+end
 ARGV.each do |root_path|
     if FileTest.file?(root_path)
         write_row.call(root_path)
