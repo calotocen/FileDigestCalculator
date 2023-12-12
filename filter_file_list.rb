7a467ec6efc273a2c0c73f8ec560321b2e27a7b8
calotocen <calotocen@gmail.com>
2023-12-12 22:53:16 +0900

    Added -g, -i, and -e option to filter_file_list.rb
    This commit changed the filter to a generic one to make it easier to use.

--- filter_file_list.rb
+++ filter_file_list.rb
@@ -3,33 +3,52 @@ require 'json'
 require 'optparse'
 
 option = {
-    key: [],
+    group_by: [],
+    include: [],
+    exclude: [],
 }
 option_parser = OptionParser.new do |op|
     op.banner = "Usage: #{$0} [options] [csv file...]"
-    op.on('-k KEY', '--key', 'search key') do |v|
-        option[:key] << v.intern
+    op.on('-g COLUMN_NAME', '--group-by') do |v|
+        option[:group_by] << v.intern
+    end
+    op.on('-i CONDITION', '--include') do |v|
+        option[:include] << v
+    end
+    op.on('-e CONDITION', '--exclude') do |v|
+        option[:exclude] << v
     end
     op.on('-o PATH', '--output', 'output file path for digests') do |v|
         option[:output] = v
     end
 end
 option_parser.parse!(ARGV)
-option[:key] << :sha256 if option[:key].empty?
 
 exit(0) if ARGV.empty?
 
-table = CSV.table(ARGV.shift)
+input_file_list = CSV.table(ARGV.shift)
 ARGV.each do |csv_path|
     CSV.table(csv_path).each do |row|
-        table << row
+        input_file_list << row
     end
 end
 
-duplicate_files = table
-    .group_by{|row| option[:key].size == 1 ? row[option[:key][0]] : option[:key].map{|key| row[key]}}
-    .select{|sha256, rows| rows.size >= 2}
+output_file_list = input_file_list.group_by{|row| option[:group_by].map{|column_name| row[column_name]}}.values
+filters = {
+    'count' => ->(rows, expression) { eval("#{rows.length}#{expression}") },
+}
+[:include, :exclude].each do |option_name|
+    option[option_name].each do |condition|
+        raise ArgumentError.new("unkonwn filter: condition='#{condition}'") unless /^(?<filter_name>[[:alnum:]]+)(:(?<parameters>.*))?$/ =~ condition
+        raise ArgumentError.new("unkonwn filter: filter_name='#{filter_name}', condition='#{condition}'") unless filters.has_key?(filter_name)
+        output_file_list.filter! do |rows|
+            result = filters[filter_name].call(rows, parameters)
+            option_name == :include ? result : !result
+        end
+    end
+end
 
+output_file_list.flatten!(1)
 writer = option[:output].nil? ? $stdout : File.open(option[:output], 'w')
-writer << CSV::Table.new(duplicate_files.values.flatten(1)).to_csv
+writer << CSV::Table.new(output_file_list).to_csv
 writer.close unless option[:output].nil?
