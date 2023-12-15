2d64e88a30addf070804c0c493ff619343278b64
calotocen <calotocen@gmail.com>
2023-12-16 03:02:05 +0900

    Formatted code

--- filter_file_list.rb
+++ filter_file_list.rb
@@ -36,7 +36,9 @@ mappers = {
     raise ArgumentError.new("unkonwn filter: condition='#{condition}'") unless /^(?<filter_name>[[:alnum:]]+)(:(?<parameters>.*))?$/ =~ condition
     raise ArgumentError.new("unkonwn filter: filter_name='#{filter_name}', condition='#{condition}'") unless mappers.has_key?(filter_name)
 end
-input_rows = ARGV.map{|csv_path| CSV.open(csv_path, converters: :all, headers: true).to_a}.flatten(1)
+input_rows = ARGV
+    .map {|csv_path| CSV.open(csv_path, converters: :all, headers: true).to_a}
+    .flatten(1)
 filtered_rows = CSV::Table.new(input_rows)
     .group_by {|row| option[:group_by].map {|column_name| row[column_name]}}
     .values
@@ -53,7 +55,11 @@ filtered_rows = CSV::Table.new(input_rows)
     .flatten(1)
 unless option[:sort_by].empty?
     sequential_number_for_stable_sort = 0
-    filtered_rows.sort_by! {|row| option[:sort_by].map{|column_name| row[column_name]} << (sequential_number_for_stable_sort += 1)}
+    filtered_rows.sort_by! do |row|
+        option[:sort_by]
+            .map {|column_name| row[column_name]}
+            .append(sequential_number_for_stable_sort += 1)
+    end
 end
 
 writer = option[:output].nil? ? $stdout : File.open(option[:output], 'w')
