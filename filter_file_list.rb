6f72b7274076316164e3bde9da1fa44c1643ba45
calotocen <calotocen@gmail.com>
2023-12-19 21:50:39 +0900

    Simplified code for filtering data

--- filter_file_list.rb
+++ filter_file_list.rb
@@ -28,30 +28,34 @@ option_parser = OptionParser.new do |op|
 end
 option_parser.parse!(ARGV)
 
-input_rows = ARGV.map{|csv_path| CSV.open(csv_path, converters: :all, headers: true).to_a}.flatten(1)
-input_file_list = CVA::Table.new(input_file_list)
-
-output_file_list = input_file_list.group_by{|row| option[:group_by].map{|column_name| row[column_name]}}.values
 mappers = {
     'count' => ->(rows, expression) {eval("#{rows.length}#{expression}") ? rows : []},
     'path' => ->(rows, regex_pattern) {rows.filter {|row| /#{regex_pattern}/ =~ row[:path]}},
 }
-[:include, :exclude].each do |option_name|
-    option[option_name].each do |condition|
-        raise ArgumentError.new("unkonwn filter: condition='#{condition}'") unless /^(?<filter_name>[[:alnum:]]+)(:(?<parameters>.*))?$/ =~ condition
-        raise ArgumentError.new("unkonwn filter: filter_name='#{filter_name}', condition='#{condition}'") unless mappers.has_key?(filter_name)
-        output_file_list.map! do |rows|
-            mapped_rows = mappers[filter_name].call(rows, parameters)
-            option_name == :include ? mapped_rows : rows - mapped_rows
-        end
-    end
+[option[:include], option[:exclude]].flatten(1) do |condition|
+    raise ArgumentError.new("unkonwn filter: condition='#{condition}'") unless /^(?<filter_name>[[:alnum:]]+)(:(?<parameters>.*))?$/ =~ condition
+    raise ArgumentError.new("unkonwn filter: filter_name='#{filter_name}', condition='#{condition}'") unless mappers.has_key?(filter_name)
 end
-
-output_file_list.flatten!(1)
+input_rows = ARGV.map{|csv_path| CSV.open(csv_path, converters: :all, headers: true).to_a}.flatten(1)
+filtered_rows = CSV::Table.new(input_rows)
+    .group_by {|row| option[:group_by].map {|column_name| row[column_name]}}
+    .values
+    .map do |rows|
+        [:include, :exclude].each do |option_name|
+            option[option_name].each do |condition|
+                /^(?<filter_name>[[:alnum:]]+)(:(?<parameters>.*))?$/ =~ condition
+                mapped_rows = mappers[filter_name].call(rows, parameters)
+                rows = (option_name == :include ? mapped_rows : rows - mapped_rows)
+            end
+        end
+        rows
+    end
+    .flatten(1)
 unless option[:sort_by].empty?
     sequential_number_for_stable_sort = 0
-    output_file_list.sort_by! {|row| option[:sort_by].map{|column_name| row[column_name]} << (sequential_number_for_stable_sort += 1)}
+    filtered_rows.sort_by! {|row| option[:sort_by].map{|column_name| row[column_name]} << (sequential_number_for_stable_sort += 1)}
 end
+
 writer = option[:output].nil? ? $stdout : File.open(option[:output], 'w')
-writer << CSV::Table.new(output_file_list).to_csv
+writer << CSV::Table.new(filtered_rows).to_csv
 writer.close unless option[:output].nil?
