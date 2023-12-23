d3331b2f8b71dcccf6eeaddced182a6028dd520e
calotocen <calotocen@gmail.com>
2023-12-23 22:46:05 +0900

    Changed to delete all files in CSV files

--- delete_files.rb
+++ delete_files.rb
@@ -1,27 +1,23 @@
-require 'json'
+require 'csv'
 require 'optparse'
 
 option = {}
 option_parser = OptionParser.new do |op|
-    op.banner = "Usage: #{$0} [options] [json file...]"
+    op.banner = "Usage: #{$0} [options] [csv file...]"
     op.on('-v', '--verbose', 'print progress') do |v|
         option[:verbose] = v
     end
 end
 option_parser.parse!(ARGV)
 
-ARGV.each do |json_path|
-    File.open(json_path) do |file_handle|
-        input_data = JSON.load(file_handle)
-        input_data['duplicate_files'].each do |key, paths|
-            paths[1..-1].each do |path|
-                begin
-                    File.delete(path)
-                    puts('delete #{path} ... done') if option[:verbose]
-                rescue Errno::ENOENT
-                    STDERR.puts("delete #{path} ... warning: #{$!}")
-                end
-            end
+ARGV.each do |csv_path|
+    CSV.foreach(csv_path, headers: true) do |row|
+        path = row['path']
+        begin
+            File.delete(path)
+            puts('delete #{path} ... done') if option[:verbose]
+        rescue Errno::ENOENT
+            STDERR.puts("delete #{path} ... warning: #{$!}")
         end
     end
 end
