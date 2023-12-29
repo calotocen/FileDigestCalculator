08212ea41c813827f30a515f72768ee2e3e4c548
calotocen <calotocen@gmail.com>
2023-12-29 22:39:09 +0900

    Change to use Logger
    It is easy to forgot to add `if option[:verbose]` to messages.

--- delete_files.rb
+++ delete_files.rb
@@ -1,5 +1,12 @@
 require 'csv'
+require 'logger'
 require 'optparse'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
+logger.formatter = ->(severity, datetime, program_name, msg) {
+    ['WARN', 'ERROR', 'FATAL'].include?(severity) ? "#{severity}: #{msg}\n" : "#{msg}\n"
+}
 
 option = {}
 option_parser = OptionParser.new do |op|
@@ -10,14 +17,19 @@ option_parser = OptionParser.new do |op|
 end
 option_parser.parse!(ARGV)
 
+logger.level = Logger::DEBUG if option[:verbose]
+
 ARGV.each do |csv_path|
     CSV.foreach(csv_path, headers: true) do |row|
         path = row['path']
         begin
             File.delete(path)
-            puts("delete #{path} ... done") if option[:verbose]
+            logger.info("deleted a file: path=\"#{path}\"")
         rescue Errno::ENOENT
-            STDERR.puts("delete #{path} ... warning: #{$!}")
+            logger.warn("file not found: path=\"#{path}\"")
+        rescue
+            logger.fatal("a fatal error occurred: path=\"#{path}\", message=\"#{$!}\"")
+            raise
         end
     end
 end
