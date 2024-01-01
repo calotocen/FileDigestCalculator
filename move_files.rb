db7d29735d6144bc780117adb6ad9f671c6ba100
calotocen <calotocen@gmail.com>
2024-01-01 15:27:46 +0900

    Changed to display message with logger

--- move_files.rb
+++ move_files.rb
@@ -1,6 +1,13 @@
 require 'csv'
+require 'logger'
 require 'optparse'
 require 'pathname'
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::WARN
+logger.formatter = ->(severity, datetime, program_name, msg) {
+    ['WARN', 'ERROR', 'FATAL'].include?(severity) ? "#{severity}: #{msg}\n" : "#{msg}\n"
+}
 
 option = {}
 option_parser = OptionParser.new do |op|
@@ -15,22 +22,28 @@ end
 option_parser.parse!(ARGV)
 exit 0 if option[:destination].nil?
 
+logger.level = Logger::DEBUG if option[:verbose]
+
 ARGV.each do |csv_path|
     CSV.foreach(csv_path, headers: true) do |row|
         src_path = row['path']
         dst_dirpath = option[:destination]
-        unless Dir.exist?(dst_dirpath)
-            print("create a directory #{path} ... ") if option[:verbose]
-            Dir.mkdir(dst_dirpath) 
-            puts('done') if option[:verbose]
-        end
-        dst_path = Pathname.join(dst_dirpath, Pathname(src_path).basename).to_s
-        if File.exist?(dst_path)
-            print('')
-        end
-        continue if File.exist?(dst_path)
+        begin
+            unless Dir.exist?(dst_dirpath)
+                Dir.mkdir(dst_dirpath) 
+                logger.info("create the directory: path=\"#{dst_dirpath}\"")
+            end
+            dst_path = Pathname.join(dst_dirpath, Pathname(src_path).basename).to_s
+            if File.exist?(dst_path)
+                logger.warn("The file already exists: path=\"#{dst_path}\"")
+                continue
+            end
 
-        path = row['path']
-        FileUtils.move(path, option[:destination])
+            FileUtils.move(src_path, dst_path)
+            logger.info("moved the file: src=\"#{src_path}\", dst=\"#{dst_path}")
+        raise
+            logger.fatal("a fatal error occurred: path=\"#{path}\", message=\"#{$!}\"")
+            raise
+        end
     end
 end
