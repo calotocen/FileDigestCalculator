ee2d80856abed8e2463750481e0e5bbe753c40f4
calotocen <calotocen@gmail.com>
2024-01-02 22:13:53 +0900

    Changed to specify a path pattern to -d option

--- move_files.rb
+++ move_files.rb
@@ -1,7 +1,6 @@
 require 'csv'
 require 'logger'
 require 'optparse'
-require 'pathname'
 
 logger = Logger.new(STDOUT)
 logger.level = Logger::WARN
@@ -12,7 +11,7 @@ logger.formatter = ->(severity, datetime, program_name, msg) {
 option = {}
 option_parser = OptionParser.new do |op|
     op.banner = "Usage: #{$0} [options] [csv file...]"
-    op.on('-d DESTINATION', '--destination', 'destination directory') do |v|
+    op.on('-d DESTINATION', '--destination', 'destination path') do |v|
         option[:destination] = v
     end
     op.on('-v', '--verbose', 'print progress') do |v|
@@ -25,24 +24,39 @@ exit 0 if option[:destination].nil?
 logger.level = Logger::DEBUG if option[:verbose]
 
 ARGV.each do |csv_path|
-    CSV.foreach(csv_path, headers: true) do |row|
+    CSV.foreach(csv_path, converters: :all, headers: true) do |row|
         src_path = row['path']
-        dst_dirpath = option[:destination]
+        dst_path = option[:destination].gsub(/{([^:}]+)(?::([^}]+))?}/) do |matched_string|
+            key = $1
+            parameter = $2
+            if row.headers.include?(key)
+                item = row[key]
+                if item.kind_of?(DateTime)
+                    item.strftime(parameter.nil? ? '%F' : parameter)
+                else
+                    item = item.to_s
+                    if parameter == 'basename'
+                        File.basename(item)
+                    end
+                end
+            end
+        end
+        if File.exist?(dst_path)
+            logger.warn("The file already exists: path=\"#{dst_path}\"")
+            continue
+        end
+
+        dst_dirpath = File.dirname(dst_path)
         begin
             unless Dir.exist?(dst_dirpath)
-                Dir.mkdir(dst_dirpath) 
+                Dir.mkdir(dst_dirpath)
                 logger.info("create the directory: path=\"#{dst_dirpath}\"")
-            end
-            dst_path = Pathname.join(dst_dirpath, Pathname(src_path).basename).to_s
-            if File.exist?(dst_path)
-                logger.warn("The file already exists: path=\"#{dst_path}\"")
-                continue
             end
 
             FileUtils.move(src_path, dst_path)
             logger.info("moved the file: src=\"#{src_path}\", dst=\"#{dst_path}")
         raise
-            logger.fatal("a fatal error occurred: path=\"#{path}\", message=\"#{$!}\"")
+            logger.fatal("a fatal error occurred: src_path=\"#{src_path}\", dst_path=\"#{dst_path}\", dst_dirpath=\"#{dst_dirpath}\", message=\"#{$!}\"")
             raise
         end
     end
