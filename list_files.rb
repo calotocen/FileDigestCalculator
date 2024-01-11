49bd93b9a87159d28e2a4ee7f5671ce39c060fc8
calotocen <calotocen@gmail.com>
2024-01-11 22:52:21 +0900

    Created symmetry between opening processing and closing one

--- list_files.rb
+++ list_files.rb
@@ -19,29 +19,31 @@ csv_options = {
     write_headers: true,
     headers: ['path', 'file_name', 'sha256', 'created_time', 'modified_time', 'access_time', 'change_time'],
 }
-csv_writer = CSV.new(option[:output].nil? ? $stdout.clone : File.open(option[:output], 'wb'), **csv_options)
-write_row = ->(path) do
-    file_stat = File.lstat(path)
-    begin
-        birth_time = file_stat.birthtime
-    rescue NotImplementedError
-        birth_time = 'N/A'
+io = option[:output].nil? ? IO.open($stdout.fileno, 'wb') : File.open(option[:output], 'wb')
+CSV.instance(io, **csv_options)
+    write_row = ->(path) do
+        file_stat = File.lstat(path)
+        begin
+            birth_time = file_stat.birthtime
+        rescue NotImplementedError
+            birth_time = 'N/A'
+        end
+        csv_writer << [File.expand_path(path),
+                    File.basename(path),
+                    Digest::SHA256.file(path).hexdigest,
+                    birth_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                    file_stat.mtime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                    file_stat.atime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                    file_stat.ctime.strftime('%Y-%m-%dT%H:%M:%S%:z')]
     end
-    csv_writer << [File.expand_path(path),
-                   File.basename(path),
-                   Digest::SHA256.file(path).hexdigest,
-                   birth_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                   file_stat.mtime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                   file_stat.atime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                   file_stat.ctime.strftime('%Y-%m-%dT%H:%M:%S%:z')]
-end
-ARGV.each do |root_path|
-    if FileTest.file?(root_path)
-        write_row.call(root_path)
-    elsif FileTest.directory?(root_path) && option[:recursive]
-        Pathname(root_path).glob('**/*') do |path|
-            write_row.call(path) if FileTest.file?(path)
+    ARGV.each do |root_path|
+        if FileTest.file?(root_path)
+            write_row.call(root_path)
+        elsif FileTest.directory?(root_path) && option[:recursive]
+            Pathname(root_path).glob('**/*') do |path|
+                write_row.call(path) if FileTest.file?(path)
+            end
         end
     end
 end
-csv_writer.close
+io.close()
