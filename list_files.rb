59a06d72f9c14da4715dae04932b9cacf486bb68
calotocen <calotocen@gmail.com>
2024-01-13 22:41:45 +0900

    Merged processing for files and one for directories

--- list_files.rb
+++ list_files.rb
@@ -17,28 +17,28 @@ option_parser.parse!(ARGV)
 
 io = option[:output].nil? ? IO.open($stdout.fileno, 'wb') : File.open(option[:output], 'wb')
 CSV.instance(io, write_headers: true, headers: %w(path file_name sha256 created_time modified_time access_time change_time)) do |csv_writer|
-    write_row = ->(path) do
-        file_stat = File.lstat(path)
-        begin
-            birth_time = file_stat.birthtime
-        rescue NotImplementedError
-            birth_time = 'N/A'
+    ARGV.each do |arg|
+        if File.file?(arg)
+            root_path = File.dirname(arg)
+            pattern = File.basename(arg)
+        elsif File.directory?(arg)
+            root_path = arg
+            pattern = '**/*'
         end
-        csv_writer << [File.expand_path(path),
-                    File.basename(path),
-                    Digest::SHA256.file(path).hexdigest,
-                    birth_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                    file_stat.mtime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                    file_stat.atime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
-                    file_stat.ctime.strftime('%Y-%m-%dT%H:%M:%S%:z')]
-    end
-    ARGV.each do |root_path|
-        if FileTest.file?(root_path)
-            write_row.call(root_path)
-        elsif FileTest.directory?(root_path) && option[:recursive]
-            Pathname(root_path).glob('**/*') do |path|
-                write_row.call(path) if FileTest.file?(path)
+        Pathname(root_path).glob(pattern) do |path|
+            file_stat = File.lstat(path)
+            begin
+                birth_time = file_stat.birthtime
+            rescue NotImplementedError
+                birth_time = 'N/A'
             end
+            csv_writer << [File.expand_path(path),
+                           File.basename(path),
+                           Digest::SHA256.file(path).hexdigest,
+                           birth_time.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                           file_stat.mtime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                           file_stat.atime.strftime('%Y-%m-%dT%H:%M:%S%:z'),
+                           file_stat.ctime.strftime('%Y-%m-%dT%H:%M:%S%:z')]
         end
     end
 end
