da07f1de71672333ded422fdac34c91694fea7c1
calotocen <calotocen@gmail.com>
2024-01-15 22:28:11 +0900

    Made the logging specifications the same as list_files.log

--- delete_files.rb
+++ delete_files.rb
@@ -2,23 +2,29 @@ require 'csv'
 require 'logger'
 require 'optparse'
 
-logger = Logger.new(STDOUT)
-logger.level = Logger::WARN
-logger.formatter = ->(severity, datetime, program_name, msg) {
-    ['WARN', 'ERROR', 'FATAL'].include?(severity) ? "#{severity}: #{msg}\n" : "#{msg}\n"
+option = {
+    log_file: STDERR,
+    log_level: Logger::WARN,
 }
-
-option = {}
 option_parser = OptionParser.new do |op|
     op.banner = "Usage: #{$0} [options] [csv file...]"
+    op.on('--log-file PATH', 'output file path for logging') do |v|
+        option[:log_file] = v
+        option[:log_level] = Logger::DEBUG unless option[:log_level].kind_of?(Integer)
+    end
+    op.on('--log-level LEVEL', /unknown|fatal|error|warn|info|debug/i, 'log level') do |v|
+        option[:log_level] = v
+    end
     op.on('-v', '--verbose', 'print progress') do |v|
         option[:verbose] = v
     end
 end
 option_parser.parse!(ARGV)
 
-logger.level = Logger::DEBUG if option[:verbose]
+logger = Logger.new(option[:log_file])
+logger.level = option[:log_level]
 
+logger.info(%(The script started: script="#{$0}, options=#{option}, args=#{ARGV}"))
 ARGV.each do |csv_path|
     CSV.foreach(csv_path, headers: true) do |row|
         path = row['path']
@@ -33,3 +39,4 @@ ARGV.each do |csv_path|
         end
     end
 end
+logger.info(%(The script finished))
