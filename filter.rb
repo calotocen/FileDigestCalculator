a833076e9fbcb21cc3f9c5059f8e8f7c9c11bd3f
calotocen <calotocen@gmail.com>
2024-02-26 21:52:00 +0900

    Supported datetime(), min(), and max()

--- filter.rb
+++ filter.rb
@@ -4,15 +4,18 @@ require 'parslet'
 class FilterParser < Parslet::Parser
     rule(:space)               { match('\s').repeat(1) }
     rule(:space?)              { space.maybe }
+    rule(:left_parenthesis)    { str('(') >> space? }
+    rule(:right_parenthesis)   { str(')') >> space? }
     rule(:identifier)          { (match('[A-Za-z]') >> match('[_0-9A-Za-z]').repeat(0)).as(:identifier) >> space? }
     rule(:integer)             { (str('-').maybe >> (str('0') | match('[1-9]') >> match('[0-9]').repeat(0))).as(:integer) >> space? }
     rule(:single_quote_string) { str("'") >> (str('\\').ignore >> match("[\\\\']") | str("'").absent? >> any).repeat.as(:single_quote_string) >> str("'") >> space? }
     rule(:double_quote_string) { str('"') >> (str('\\').ignore >> match('[\\\\"]') | str('"').absent? >> any).repeat.as(:double_quote_string) >> str('"') >> space? }
     rule(:regular_expression)  { str('/') >> (str('\\') >> any | str('/').absent? >> any).repeat.as(:regular_expression) >> str('/') >> space?  }
     rule(:string)              { single_quote_string | double_quote_string }
+    rule(:function_call)       { identifier.as(:function_name) >> left_parenthesis >> (integer | string | identifier).as(:argument) >> right_parenthesis }
     rule(:binary_operator)     { (match('[<>]') >> match('=').maybe | match('[!=]') >> match('[=~]')).as(:binary_operator) >> space? }
     rule(:lhs)                 { identifier }
-    rule(:rhs)                 { integer | string | regular_expression }
+    rule(:rhs)                 { integer | string | regular_expression | function_call }
     rule(:filter)              { lhs.as(:lhs) >> (binary_operator.as(:operator) >> rhs.as(:rhs)).maybe }
     root :filter
 end
@@ -25,16 +28,36 @@ class FilterTransform < Parslet::Transform
     rule(:binary_operator => simple(:binary_operator))         { binary_operator.to_s }
     rule(:lhs => simple(:lhs))                                 { ->(rows) { rows.filter {|row| !row[lhs].nil?} } }
     rule(:rhs => simple(:rhs))                                 { rhs }
+    rule(:function_name => simple(:function_name), :argument => simple(:argument)) do
+        case function_name
+        when 'datetime'
+            DateTime.parse(argument)
+        when 'min', 'max'
+            ->(rows) { rows.map{|row| row[argument]}.method(function_name).call() }
+        end
+    end
     rule(:lhs => simple(:lhs), :operator => simple(:operator), :rhs => simple(:rhs)) do
-        method = rhs.method(operator.tr('<>', '><').intern)
-        case lhs
-        when 'index'
+        if rhs.instance_of?(Proc)
+            # the operator must be reversed to swap the left and right operands.
+            # rows specified for the following lambda must not be empty.
+            method_factory = ->(rows) { rhs.call(rows).method(operator.tr('<>', '><').intern) }
             lambda do |rows|
-                index = -1
-                rows.filter {method[index += 1]}
+                return [] if rows.empty?
+                method = method_factory.call(rows)
+                rows.filter {|row| method.call(row[lhs])}
             end
         else
-            ->(rows) { rows.filter {|row| method[row[lhs]]} }
+            # the operator must be reversed to swap the left and right operands.
+            method = rhs.method(operator.tr('<>', '><').intern)
+            case lhs
+            when 'index'
+                lambda do |rows|
+                    index = -1
+                    rows.filter { method.call(index += 1) }
+                end
+            else
+                ->(rows) { rows.filter {|row| method.call(row[lhs])} }
+            end
         end
     end
 end
