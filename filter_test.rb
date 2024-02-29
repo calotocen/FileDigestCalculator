7204042c72398d127b6f2b146c84f29a6581bd33
calotocen <calotocen@gmail.com>
2024-02-29 22:04:30 +0900

    Deleted an unnecessary comment

--- filter_test.rb
+++ filter_test.rb
@@ -2,45 +2,6 @@ require 'csv'
 require 'minitest/autorun'
 require './filter.rb'
 
-=begin
-csv_text = <<-EOS
-number,text,datetime,mix
-1,abcdefg,2024-02-03 11:23:34,3
-2,bcdefghi,2024-03-03 13:24:34,zxcv
-3,cdefgh,2024-04-03 15:33:34,2024-03-03
-4,def,2024-05-03 17:21:34,11:23:44
-5,efgh,2024-06-03 19:22:34,
-99,f,2024-08-03 19:22:34,
-EOS
-csv = CSV::Table.new(CSV.new(csv_text.dup, headers: true, converters: :all).to_a)
-rows = csv.each.to_a
-
-test_cases = [
-    { filter: %q(mix), expected: rows[0..3] },
-    { filter: %q(number == 2), expected: rows[1..1] },
-    { filter: %q(number > 1), expected: rows[1..5] },
-    { filter: %q(number > 0), expected: rows[0..5] },
-    { filter: %q(number < 99), expected: rows[0..4] },
-    { filter: %q(text == "abcdefg"), expected: rows[0..0] },
-    { filter: %q(text != 'abcdefg'), expected: rows[1..5] },
-    { filter: %q(text =~ /def/), expected: rows[0..3] },
-    { filter: %q(text =~ /^c/), expected: rows[2..2] },
-    { filter: %q(datetime == datetime('2024-02-03 11:23:34')), expected: rows[0..0] },
-    { filter: %q(datetime < datetime('2024-05-03')), expected: rows[0..2] },
-    { filter: %q(number == min(number)), expected: rows[0..0] },
-    { filter: %q(number == max(number)), expected: rows[5..5] },
-    { filter: %q(count == 6), expected: rows[0..5] },
-]
-parser = FilterParser.new
-transform = FilterTransform.new
-test_cases.each do |test_case|
-    r = parser.parse(test_case[:filter])
-    l = transform.apply(r)
-    filtered_rows = l.call(rows)
-    p filtered_rows == test_case[:expected]
-end
-=end
-
 class FilterTest < Minitest::Test
     def test_case_of_specifying_only_column_name
         rows = CSV::Table.new(CSV.new(<<~'EOS', headers: true, converters: :all).to_a).each.to_a
